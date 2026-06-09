fee85589380ed9b4f8c0302b37df1cb52550641c
FedeCoyra <federicocoyra@gmail.com>
2026-06-09 01:46:41 -0300

    Corecciones

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://34.63.158.31:8000/api/:path*',
-      },
-      {
-        source: '/minio/:path*',
-        destination: 'http://35.194.31.183:9000/:path*',
-      }
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
